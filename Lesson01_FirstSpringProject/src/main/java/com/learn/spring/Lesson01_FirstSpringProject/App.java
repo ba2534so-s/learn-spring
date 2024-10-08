82d21e2225e6fdaf3e00c5db9fd168ba06258a80
Baran <Baran.soysuren@gmail.com>
2024-10-08 20:34:20 +0200

    create maven project for learning spring

---
+++ Lesson01_FirstSpringProject/src/main/java/com/learn/spring/Lesson01_FirstSpringProject/App.java
@@ -0,0 +1,13 @@
+package com.learn.spring.Lesson01_FirstSpringProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
