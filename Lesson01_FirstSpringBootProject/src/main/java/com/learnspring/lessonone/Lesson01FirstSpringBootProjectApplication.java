a7b76fe3d988f706ee57b7774660c8baed898808
Baran <Baran.soysuren@gmail.com>
2024-10-08 21:01:29 +0200

    create spring starter project for first spring boot lesson

---
+++ Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Lesson01FirstSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.lessonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson01FirstSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson01FirstSpringBootProjectApplication.class, args);
+	}
+
+}
