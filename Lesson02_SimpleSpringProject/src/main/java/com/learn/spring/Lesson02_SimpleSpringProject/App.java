6dda204171bc8bc323d1bd705a565625876b0fb0
Baran <Baran.soysuren@gmail.com>
2024-10-09 17:40:27 +0200

    create instance of ApplicationContext

--- Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/App.java
+++ Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/App.java
@@ -2,6 +2,8 @@ package com.learn.spring.Lesson02_SimpleSpringProject;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -13,6 +15,7 @@ public class App
     public static void main( String[] args )
     {
     	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("beans.xml"));
+    	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
     	
         Developer obj = (Developer) factory.getBean("developer");
         obj.code();
