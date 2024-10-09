4df674480d5b98148f2cdc77dd721423cc7117e7
Baran <Baran.soysuren@gmail.com>
2024-10-09 21:36:51 +0200

    create constructor

--- Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Laptop.java
+++ Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Laptop.java
@@ -2,6 +2,11 @@ package com.learn.spring.Lesson02_SimpleSpringProject;
 
 public class Laptop {
 	
+	
+	public Laptop() {
+		
+	}
+	
 	public void compile() {
 		System.out.println("Compiling code...");
 	}
