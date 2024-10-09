8a8356273452f9abf4161c411cd03d55c821ef76
Baran <Baran.soysuren@gmail.com>
2024-10-09 12:45:21 +0200

    declare developer object

--- Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Lesson01FirstSpringBootProjectApplication.java
+++ Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Lesson01FirstSpringBootProjectApplication.java
@@ -8,6 +8,8 @@ public class Lesson01FirstSpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson01FirstSpringBootProjectApplication.class, args);
+		
+		Developer obj;
 	}
 
 }
