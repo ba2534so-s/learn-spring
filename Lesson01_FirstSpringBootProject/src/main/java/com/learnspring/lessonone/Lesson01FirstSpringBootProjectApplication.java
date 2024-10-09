b1a253a7670e9699bce45120805b7ca6a8f394d3
Baran <Baran.soysuren@gmail.com>
2024-10-09 12:48:35 +0200

    declare ApplicationContext

--- Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Lesson01FirstSpringBootProjectApplication.java
+++ Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Lesson01FirstSpringBootProjectApplication.java
@@ -2,14 +2,17 @@ package com.learnspring.lessonone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lesson01FirstSpringBootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson01FirstSpringBootProjectApplication.class, args);
+		ApplicationContext context = SpringApplication.run(Lesson01FirstSpringBootProjectApplication.class, args);
 		
 		Developer obj;
+		
+		
 	}
 
 }
