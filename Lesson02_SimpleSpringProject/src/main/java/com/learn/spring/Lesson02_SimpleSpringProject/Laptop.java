3a3e41f7268e5ff94daf7b6acafeaeed0d0d035d
Baran <Baran.soysuren@gmail.com>
2024-10-09 21:48:09 +0200

    add memory attribute to laptop

--- Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Laptop.java
+++ Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Laptop.java
@@ -2,10 +2,14 @@ package com.learn.spring.Lesson02_SimpleSpringProject;
 
 public class Laptop {
 	
+	private int memory;
 	
 	public Laptop() {
 		System.out.println("Creating laptop...");
 	}
+	
+	
+	
 	
 	public void compile() {
 		System.out.println("Compiling code...");
