ae43e04264e6a02e446bd98e537cc9d5f10fee60
Baran <Baran.soysuren@gmail.com>
2024-10-09 17:44:51 +0200

    add constructor to Developer class

--- Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Developer.java
+++ Lesson02_SimpleSpringProject/src/main/java/com/learn/spring/Lesson02_SimpleSpringProject/Developer.java
@@ -2,6 +2,10 @@ package com.learn.spring.Lesson02_SimpleSpringProject;
 
 public class Developer {
 	
+	public Developer() {
+		
+	}
+	
 	public void code() {
 		System.out.println("I am coding...");
 	}
