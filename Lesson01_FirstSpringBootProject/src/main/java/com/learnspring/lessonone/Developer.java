ff2ec77098248ae1416c77e081cece99eab22cab
Baran <Baran.soysuren@gmail.com>
2024-10-09 12:56:34 +0200

    add autowired annotation

--- Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Developer.java
+++ Lesson01_FirstSpringBootProject/src/main/java/com/learnspring/lessonone/Developer.java
@@ -1,10 +1,12 @@
 package com.learnspring.lessonone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Developer {
 	
+	@Autowired
 	Laptop lap;
 	
 	public void code() {
