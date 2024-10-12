85cb65ef5c93ed4f6d9e77d04ba92b1c2b22adf9
Baran <Baran.soysuren@gmail.com>
2024-10-12 09:55:52 +0200

    Create instance of developer

--- Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/Lesson03SpringBootJdbcApplication.java
+++ Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/Lesson03SpringBootJdbcApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.learnspring.jdbc.model.Developer;
+
 @SpringBootApplication
 public class Lesson03SpringBootJdbcApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Lesson03SpringBootJdbcApplication.class, args);
+		
+		Developer dev1 = context.getBean(Developer.class);
 	}
 
 }
