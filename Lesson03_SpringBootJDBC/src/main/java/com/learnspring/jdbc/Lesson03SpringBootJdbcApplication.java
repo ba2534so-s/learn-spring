5d36cca18b6f478c0e806eb3db291ec942ef2ed5
Baran <Baran.soysuren@gmail.com>
2024-10-12 09:53:14 +0200

    create variable of ApplicationContext

--- Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/Lesson03SpringBootJdbcApplication.java
+++ Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/Lesson03SpringBootJdbcApplication.java
@@ -2,12 +2,13 @@ package com.learnspring.jdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lesson03SpringBootJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson03SpringBootJdbcApplication.class, args);
+		ApplicationContext context = SpringApplication.run(Lesson03SpringBootJdbcApplication.class, args);
 	}
 
 }
