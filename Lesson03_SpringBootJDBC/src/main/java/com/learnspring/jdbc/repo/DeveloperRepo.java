965b1ffa25a9de08e5c1803cc18cf3ac41508216
Baran <Baran.soysuren@gmail.com>
2024-10-12 10:08:44 +0200

    add repository annotation

--- Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
+++ Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
@@ -3,8 +3,12 @@ package com.learnspring.jdbc.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.learnspring.jdbc.model.Developer;
 
+
+@Repository
 public class DeveloperRepo {
 	
 	public void save(Developer developer) {
@@ -12,7 +16,7 @@ public class DeveloperRepo {
 	}
 	
 	public List<Developer> getAll() {
-		return new ArrayList()<>();
+		return new ArrayList<>();
 	}
 
 }
