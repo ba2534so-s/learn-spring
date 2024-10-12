535a72c4233df9a39a3cacfdcb71f9d55b2f3adb
Baran <Baran.soysuren@gmail.com>
2024-10-12 10:31:03 +0200

    create an instance of JdbcTemplate

--- Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
+++ Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
@@ -3,6 +3,7 @@ package com.learnspring.jdbc.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.learnspring.jdbc.model.Developer;
@@ -10,6 +11,8 @@ import com.learnspring.jdbc.model.Developer;
 
 @Repository
 public class DeveloperRepo {
+	
+	private JdbcTemplate template;
 	
 	public void save(Developer developer) {
 		
