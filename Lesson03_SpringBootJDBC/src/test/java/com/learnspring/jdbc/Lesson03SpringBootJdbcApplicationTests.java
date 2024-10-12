560ba4c72824cdbb93dae96841d6d71853b663dc
Baran <Baran.soysuren@gmail.com>
2024-10-12 09:46:13 +0200

    create spring boot project for learning jdbc

---
+++ Lesson03_SpringBootJDBC/src/test/java/com/learnspring/jdbc/Lesson03SpringBootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson03SpringBootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
