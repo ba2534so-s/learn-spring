66ee9448114173169e3444a3d8506aac477315b0
Baran <Baran.soysuren@gmail.com>
2024-10-12 10:04:35 +0200

    create getAll method signature

--- Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
+++ Lesson03_SpringBootJDBC/src/main/java/com/learnspring/jdbc/repo/DeveloperRepo.java
@@ -1,10 +1,16 @@
 package com.learnspring.jdbc.repo;
 
+import java.util.List;
+
 import com.learnspring.jdbc.model.Developer;
 
 public class DeveloperRepo {
 	
 	public void save(Developer developer) {
+		
+	}
+	
+	public List<Developer> getAll() {
 		
 	}
 
