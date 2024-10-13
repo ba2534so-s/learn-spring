e2e3238285ffff05e639edf17810caec8bcd0237
Baran <Baran.soysuren@gmail.com>
2024-10-13 09:44:45 +0200

    add semicolon

--- Lesson03_SpringBootJDBC/src/main/resources/data.sql
+++ Lesson03_SpringBootJDBC/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO developers (developer_id, name, tech) VALUES (101, 'John Smith', 'Blockchain');
 INSERT INTO developers (developer_id, name, tech) VALUES (102, 'Andy Anderson', 'IoT');
-INSERT INTO developers (developer_id, name, tech) VALUES (103, 'Emma Erikson', 'Python')
+INSERT INTO developers (developer_id, name, tech) VALUES (103, 'Emma Erikson', 'Python');
