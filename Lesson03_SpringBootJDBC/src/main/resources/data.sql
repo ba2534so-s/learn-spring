b959044c96567a7558fb1fd3761b8f54ca3e3b80
Baran <Baran.soysuren@gmail.com>
2024-10-13 09:43:08 +0200

    add another developer

--- Lesson03_SpringBootJDBC/src/main/resources/data.sql
+++ Lesson03_SpringBootJDBC/src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO developers (developer_id, name, tech) VALUES (101, 'John Smith', 'Blockchain')
+INSERT INTO developers (developer_id, name, tech) VALUES (101, 'John Smith', 'Blockchain');
+INSERT INTO developers (developer_id, name, tech) VALUES (102, 'Andy Anderson', 'IoT');
