1878318c9de8a01454e6c067e8d1c6ed5db377c9
Baran <Baran.soysuren@gmail.com>
2024-10-13 10:33:59 +0200

    create spring boot web project to learn mvc

---
+++ Lesson04_SpringBootMVC/src/test/java/com/learnspring/mvc/Lesson04SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson04SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
