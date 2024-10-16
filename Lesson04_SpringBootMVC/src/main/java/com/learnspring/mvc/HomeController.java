979dc94e20b716564a7978aa14ab4387f6e13b25
Baran <Baran.soysuren@gmail.com>
2024-10-16 10:09:21 +0200

    return index page

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/")
-	public void home() {
-		System.out.println("Home page requested");
+	public String home() {
+		return "index.jsp";
 	}
 
 }
