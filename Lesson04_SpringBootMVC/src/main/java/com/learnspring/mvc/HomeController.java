74ee99b8ee66f86dc7c895e0b06e7ad20768492a
Baran <Baran.soysuren@gmail.com>
2024-10-16 10:05:58 +0200

    add request mapping annotation

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -1,10 +1,12 @@
 package com.learnspring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+	@RequestMapping("/")
 	public void home() {
 		System.out.println("Home page requested");
 	}
