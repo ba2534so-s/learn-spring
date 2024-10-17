2fe880d5a650d392a28b50d569f16f16b5829d4a
Baran <Baran.soysuren@gmail.com>
2024-10-17 10:27:38 +0200

    add Model as parameter

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -31,9 +32,7 @@ public class HomeController {
 	*/
 
 	@RequestMapping("/add")
-	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
-		ModelAndView mv = new ModelAndView("result");
-		//mv.setViewName("result");
+	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
 		
 		int result = i + j;
 		mv.addObject("result", result);
