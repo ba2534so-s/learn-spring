e0b766605ceb4edddf46e73a1980190eb138af6d
Baran <Baran.soysuren@gmail.com>
2024-10-17 09:49:22 +0200

    set view name

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	@RequestMapping("/add")
 	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
 		ModelAndView mv = new ModelAndView();
-		
+		mv.setViewName("result.jsp");
 		
 		int result = i + j;
 		
