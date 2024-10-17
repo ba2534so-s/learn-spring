957198d3608a8efcf2889dad5a5523b1159c919d
Baran <Baran.soysuren@gmail.com>
2024-10-17 10:28:38 +0200

    add ModelMap parameter and change method to addAttribute

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -1,6 +1,7 @@
 package com.learnspring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,10 +33,10 @@ public class HomeController {
 	*/
 
 	@RequestMapping("/add")
-	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
+	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, ModelMap m) {
 		
 		int result = i + j;
-		mv.addObject("result", result);
+		m.addAttribute("result", result);
 		
 		return "result";
 	}
