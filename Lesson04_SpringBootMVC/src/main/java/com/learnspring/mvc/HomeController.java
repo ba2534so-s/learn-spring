00bcb8d484f194413e5947d90d8c07b895e9a8a9
Baran <Baran.soysuren@gmail.com>
2024-10-17 09:48:26 +0200

    create instance of ModelAndView

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -3,6 +3,7 @@ package com.learnspring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +18,7 @@ public class HomeController {
 	
 	@RequestMapping("/add")
 	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
+		ModelAndView mv = new ModelAndView();
 		
 		int result = i + j;
 		
