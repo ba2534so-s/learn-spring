e4c3ab1afda3f67a3381095415881be8c3affd01
Baran <Baran.soysuren@gmail.com>
2024-10-17 09:44:42 +0200

    add HttpSession as parameter

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/add")
-	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
+	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, HttpSession session) {
 		
 		int result = i + j;
 		
