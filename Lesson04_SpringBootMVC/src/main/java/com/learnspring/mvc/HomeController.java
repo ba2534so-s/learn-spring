ee9284795bfe2b820244f3df400a055901e20eb7
Baran <Baran.soysuren@gmail.com>
2024-10-18 10:18:52 +0200

    add @ModelAttribute annotation

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -2,6 +2,7 @@ package com.learnspring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -44,7 +45,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/addDeveloper")
-	public String addDeveloper(Developer d, Model m) {
+	public String addDeveloper(@ModelAttribute Developer d, Model m) {
 		
 		
 		return "result";
