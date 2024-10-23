ddb648a3876a74ddcd4a6be2a03650f005d16448
Baran <Baran.soysuren@gmail.com>
2024-10-23 12:01:11 +0200

    add GetMapping annotation

--- Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson04_SpringBootMVC/src/main/java/com/learnspring/mvc/HomeController.java
@@ -3,6 +3,7 @@ package com.learnspring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,6 +61,8 @@ public class HomeController {
 		
 	}
 	
+	
+	@GetMapping("getDevelopers")
 	public String getDevelopers() {
 		
 		
