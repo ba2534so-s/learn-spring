40a0b13f2051dd0d161857c4e3db444ed347e5c6
Baran <Baran.soysuren@gmail.com>
2024-11-05 22:22:30 +0100

    add model attribute annotation

--- Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
@@ -1,8 +1,11 @@
 package com.learnspring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
+	
+	@ModelAttribute
 
 }
