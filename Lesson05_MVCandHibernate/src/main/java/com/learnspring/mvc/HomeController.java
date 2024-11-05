5229a40a59d8172c314acb19510b6d949446c8ff
Baran <Baran.soysuren@gmail.com>
2024-11-05 22:23:18 +0100

    ad modelData method signature

--- Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
@@ -1,11 +1,14 @@
 package com.learnspring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
 	
 	@ModelAttribute
-
+	public void modelData(Model m) {
+		
+	}
 }
