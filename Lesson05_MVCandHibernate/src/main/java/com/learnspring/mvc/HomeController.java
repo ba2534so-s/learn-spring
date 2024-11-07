2e577a624c7879d3bb1ddbd0fc9d1fd03e2e36bf
Baran <Baran.soysuren@gmail.com>
2024-11-07 12:26:44 +0100

    call get developers

--- Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
@@ -39,11 +39,7 @@ public class HomeController {
 	
 	@GetMapping("getDevelopers")
 	public String getDevelopers(Model m) {
-		List<Developer> developers = Arrays.asList(
-				new Developer(101, "Mary Jane Johnson"),
-				new Developer(102, "Bruce Wayne"),
-				new Developer(103, "Peter Parker"));
-		m.addAttribute("developers", developers);
+		m.addAttribute("developers", dao.getDevelopers());
 		return "showDevelopers";
 	}
 }
