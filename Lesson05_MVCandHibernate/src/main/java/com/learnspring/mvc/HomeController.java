3aa711997db149e5e32276888be9f3cb6fd77b5f
Baran <Baran.soysuren@gmail.com>
2024-11-07 12:25:36 +0100

    add autowired annotation

--- Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
+++ Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/HomeController.java
@@ -3,6 +3,7 @@ package com.learnspring.mvc;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import com.learnspring.mvc.model.Developer;
 @Controller
 public class HomeController {
 	
+	@Autowired
 	DeveloperDao dao;
 	
 	@ModelAttribute
