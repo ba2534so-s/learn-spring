9df4561e582056feba6869962492dea2fccff216
Baran <Baran.soysuren@gmail.com>
2024-11-07 12:07:50 +0100

    create getDevelopers method signature

--- Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/dao/DeveloperDao.java
+++ Lesson05_MVCandHibernate/src/main/java/com/learnspring/mvc/dao/DeveloperDao.java
@@ -1,9 +1,17 @@
 package com.learnspring.mvc.dao;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
+
+import com.learnspring.mvc.model.Developer;
 
 public class DeveloperDao {
 	
 	private SessionFactory sessionFactory; 
+	
+	public List<Developer> getDevelopers() {
+		
+	}
 
 }
